4712ffc6829537f90d02a49e468b66263b600c5c
Nikolay Petrov <sungon@gmail.com>
2024-10-20 19:43:23 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "klev-cli is a cli to use klev.dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+        };
+      });
+}
